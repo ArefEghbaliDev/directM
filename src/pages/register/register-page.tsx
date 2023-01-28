315e725e047758dc922eeb9fb82a2fffdf27f6d2
Aref <arefeghbalidev@outlook.com>
2023-01-28 13:01:55 +0330

    Change register page ui

--- src/pages/register/register-page.tsx
+++ src/pages/register/register-page.tsx
@@ -1,23 +1,27 @@
-import {createSignal} from 'solid-js';
-import {useNavigate} from '@solidjs/router';
+import { createSignal } from 'solid-js';
+import { useNavigate } from '@solidjs/router';
+import {Icon} from '@amoutonbrady/solid-heroicons';
+import {paperAirplane} from '@amoutonbrady/solid-heroicons/outline';
 
 export default function RegisterPage() {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-  const [peerId,setPeerId] = createSignal<string>("");
+  const [peerId, setPeerId] = createSignal<string>("");
 
   const handleContinue = () => {
-      localStorage.setItem("id",peerId());
-      navigate('/inbox', {replace: true});
+    if(peerId() === "") return;
+
+    localStorage.setItem("id", peerId());
+    navigate('/inbox', { replace: true });
   }
-    return (
-    <div>
-      <h2>Your ID</h2>
-      <p>{peerId()}</p>
-      <h2>Enter Peer ID</h2>
+  return (
+    <div class='p-5'>
+      <h2 class='font-bold text-xl'>Register</h2>
       <p>Choose an ID to use so you and others can connect to each other</p>
-      <input type="text" name="peer-id" value={peerId()} onInput={(e) => {setPeerId(e.currentTarget.value)}} />
-      <button type="button" onClick={handleContinue}>Continue</button>
+      <div class='flex items-center justify-between rounded-full border border-gray-200 p-3 mt-8'>
+      <input type="text" name="peer-id" value={peerId()} placeholder='Enter a ID' onInput={(e) => { setPeerId(e.currentTarget.value) }} class='flex-1 focus:outline-none' />
+      <button type="button" onClick={handleContinue} class="rotate-90"><Icon path={paperAirplane} width={24} height={24} /></button>
+      </div>
     </div>
-    )
+  )
 }
